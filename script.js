84e0a8c2c0adbe4307f73bc5d70bbc7e697e7f6d
zahon <zahongirabdulhamidov@gmail.com>
2021-09-18 02:34:17 +0500

    to do list

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.text');
+const buttun = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+buttun.addEventListener('click', e => {
+	if (input.value !== '') return createDeleteElements(), (input.value = '');
+});
+
+function createDeleteElements(value) {
+	const li = document.createElement('li');
+	const btn = document.createElement('img');
+
+	li.className = 'li';
+	li.textContent = input.value;
+	btn.className = 'delete';
+	li.appendChild(btn);
+
+	btn.addEventListener('click', l => {
+		result.removeChild(li);
+	});
+	result.appendChild(li);
+}
